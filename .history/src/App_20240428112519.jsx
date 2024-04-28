a94db7da0c3c39c0ad01ae1bb717eef3578cc560
Artit1515 <arthit.lue@dome.tu.ac.th>
2024-04-28 11:30:57 +0700

    merge develop to feature

---
+++ .history/src/App_20240428112519.jsx
@@ -0,0 +1,43 @@
+import Navbar from "./Component/Navbar/Navbar"
+import Home from "./Page/Home/Home"
+import Login from "./Page/Auth/Login"
+import SignUp from "./Page/Auth/SignUp"
+import Trending from "./Page/Home/Trending/Trending"
+import {  Routes, Route } from "react-router-dom";
+import Footer from "./Component/Navbar/Footer"
+import New from "./Page/New/New"
+import Cart from "./Page/Ecommerce/Cart"
+import FigureDetails from "./Page/Figure/FigureDetails"
+import Admin from "./Page/Admin/Admin"
+<<<<<<< HEAD
+import EditFigure from "./Page/EditFigure/EditFigure"
+=======
+import CheckOut from "./Page/Confirm/CheckOut"
+>>>>>>> Feature
+function App() {
+  return (
+    <>
+    <Navbar />
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/trending" element={<Trending />} />
+        <Route path ="/new" element={<New />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/figure/:id" element={<FigureDetails />} />
+        <Route path="/admin" element={<Admin />} />
+<<<<<<< HEAD
+        <Route path="/editfigure" element={<EditFigure />} />
+=======
+        <Route path="/checkout" element={<CheckOut />} />
+>>>>>>> Feature
+    </Routes>
+   
+    
+    </>
+  );
+}
+
+
+export default App
