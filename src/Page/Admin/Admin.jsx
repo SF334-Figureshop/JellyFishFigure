0275c58cbcc808c816da2ec881a02cd1ca018858
jakkapong <jakkapong.boo@dome.tu.ac.th>
2024-04-29 11:07:52 +0700

    change document naming from figure1 to figure00001

--- src/Page/Admin/Admin.jsx
+++ src/Page/Admin/Admin.jsx
@@ -62,12 +62,12 @@ const Admin = () => {
 
   useEffect(()=>{
     if(figures[0] && Number(figures[0].id.substring(6)) !== 1 || figures.length <1){
-      setnewFigureURL(`Figure1`) 
+      setnewFigureURL(`Figure0001`) 
       
     } else {
       for(let i = 0 ; i < figures.length-1 ; i++){
         if(Number(figures[i].id.substring(6))+1 !== Number(figures[i+1].id.substring(6))) {
-          setnewFigureURL(`Figure${Number(figures[i].id.substring(6))+1}`);
+          setnewFigureURL(`Figure${(Number(figures[i].id.substring(6))+1).toString().padStart(5,"0")}`);
           break;
         }
         setnewFigureURL(`Figure${allItemType+1}`);
