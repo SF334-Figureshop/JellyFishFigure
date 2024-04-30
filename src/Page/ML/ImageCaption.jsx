4e1466c8ba4bcff8293aecdb9e33d6668d59506a
Artit1515 <arthit.lue@dome.tu.ac.th>
2024-04-30 09:52:21 +0700

    feat : complete Tag generator

--- src/Page/ML/ImageCaption.jsx
+++ src/Page/ML/ImageCaption.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
+import { Button, Container, FormControl, InputLabel, ListItem, TextField, Typography } from "@material-ui/core";
 
 export default function TagGenerator() {
   const [text, setText] = useState("");
@@ -29,15 +30,35 @@ export default function TagGenerator() {
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" onChange={(e) => setText(e.target.value)} />
-        <button type="submit">Submit</button>
-      </form>
+      <Container maxWidth="xl" >
+        <Typography variant="h2" gutterBottom>Tag Generator</Typography>
+        <form onSubmit={handleSubmit}>
+          <FormControl fullWidth>
+            <TextField
+              required
+              variant="outlined"
+              multiline
+              rows={20}
+              id="Figure Description"
+              label="Figure Description"
+              onChange={(e) => setText(e.target.value)}
+              inputProps={{ maxLength: 5000 }}
+            />
+            <TextField variant="outlined" type="number" id="numtag" value={numtag} onChange={(e)=> setNumtag(e.target.value)} inputProps={{ min: 1, max: 10 }}/>
+            <Button color="primary" variant="contained" type="submit">
+              Generate Tag
+            </Button>
+          </FormControl>
+        </form>
+      </Container>
       <div>
+        <Container>
         {tag.map((tags, index) => (
-          <div key={index}>{tags.tag}</div>
+          <ListItem  key={index}>{tags.tag}</ListItem>
         ))}
+        </Container>
       </div>
     </div>
+    
   );
 }
